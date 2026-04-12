b386ebf6bf47aa443d3737186e61c995c5102824
sunghyeon jeon <jeon28in@gmail.com>
2026-04-12 21:27:22 +0900

    feat(DAL-49): 구글/소셜 가입 추가 정보 입력 페이지 구현

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -288,7 +288,7 @@ export default function SignupPage() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider,
                 options: {
-                    redirectTo: `${window.location.origin}/`,
+                    redirectTo: `${window.location.origin}/signup/complete`,
                 }
             });
             if (error) throw error;
